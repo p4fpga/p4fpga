3f5395b5174122bd03cb770332181e16db5b15c2
Han Wang <hwang@cs.cornell.edu>
2016-03-31 01:12:58 -0400

    BIR: generate struct and table

--- p4c_fpga/start.py
+++ p4c_fpga/start.py
@@ -4,37 +4,140 @@
 '''
 
 import argparse
+import math
+import sys
+import yaml
 from collections import OrderedDict
 from p4_hlir.main import HLIR
 from p4_hlir.hlir import p4_parse_state, p4_action, p4_table, p4_header_instance
+
+# Print OrderedDict() to standard yaml
+# from http://blog.elsdoerfer.name/2012/07/26/make-pyyaml-output-an-ordereddict/
+def represent_odict(dump, tag, mapping, flow_style=None):
+    '''Like BaseRepresenter.represent_mapping, but does not issue the sort() '''
+    value = []
+    node = yaml.MappingNode(tag, value, flow_style=flow_style)
+    if dump.alias_key is not None:
+        dump.represented_objects[dump.alias_key] = node
+        best_style = True
+    if hasattr(mapping, 'items'):
+        mapping = mapping.items()
+    for item_key, item_value in mapping:
+        node_key = dump.represent_data(item_key)
+        node_value = dump.represent_data(item_value)
+        if not (isinstance(node_key, yaml.ScalarNode) and not node_key.style):
+            best_style = False
+        if not (isinstance(node_value, yaml.ScalarNode) and not node_value.style):
+            best_style = False
+        value.append((node_key, node_value))
+    if flow_style is None:
+        if dump.default_flow_style is not None:
+            node.flow_style = dump.default_flow_style
+        else:
+            node.flow_style = best_style
+    return node
 
 # Class definition derived from bir_meta.yaml
 class Struct(object):
     ''' struct '''
     required_attributes = ['fields']
-    def __init__(self, hlirHeader):
-        if isinstance(hlirHeader, p4_header_instance):
-            self.fields = OrderedDict()
-            for tmp_field in hlirHeader.fields:
-                self.fields[tmp_field.name] = tmp_field.width
-        elif isinstance(hlirHeader, p4_table):
-            print 'request'
-            print 'response'
-            pass
+    def __init__(self, hlir, **kwargs):
+        assert (isinstance(hlir, p4_header_instance)
+                or isinstance(hlir, p4_table))
+
+        self.hlir = hlir
+        self.name = hlir.name # set default name
+        self.kwargs = kwargs
+        self.fields = []
+
+        if isinstance(hlir, p4_header_instance):
+            self.build_header()
+        elif isinstance(hlir, p4_table):
+            print kwargs['field']
+            self.build_table(kwargs['field'])
+
+    def build_table(self, field_width=None):
+        ''' build table request and response struct '''
+        if self.kwargs['type'] == 'request':
+            self.name = self.hlir.name + "_req_t"
+            for field in self.hlir.match_fields:
+                p4_field = field[0]
+                #p4_match_type = field[1]
+                self.fields.append({p4_field.name : field_width[p4_field.name]})
+        elif self.kwargs['type'] == 'response':
+            self.name = self.hlir.name + "_resp_t"
+            self.fields.append({'hit': 1})
+            p4_action_width = int(math.ceil(math.log(len(self.hlir.actions)+1)))
+            self.fields.append({'p4_action': p4_action_width})
+            for index, action in enumerate(self.hlir.actions):
+                action_name = "action_{}_arg{}".format(index, 0)
+                self.fields.append({action_name : sum(action.signature_widths)})
+
+    def build_header(self):
+        ''' build header struct '''
+        for tmp_field in self.hlir.fields:
+            self.fields.append({tmp_field.name: tmp_field.width})
+
+    def dump(self):
+        ''' dump struct to yaml '''
+        dump = OrderedDict()
+        dump['type'] = 'struct'
+        dump['fields'] = self.fields
+        return dump
 
 class Metadata(object):
     ''' metadata '''
     required_attributes = ['values', 'visibility', 'value_inits']
-    def __init__(self, hlirMetadata):
-        print hlirMetadata
+    def __init__(self, hlir):
+        assert isinstance(hlir, p4_header_instance)
+        self.hlir = hlir
+        self.name = hlir.name # set default name
+        self.fields = []
+        if isinstance(hlir, p4_header_instance):
+            self.build_metadata()
+
+    def build_metadata(self):
+        ''' build metadata struct '''
+        for tmp_field in self.hlir.fields:
+            self.fields.append({tmp_field.name: tmp_field.width})
+
+    def dump(self):
+        ''' dump metadata to yaml '''
+        dump = OrderedDict()
+        dump['type'] = 'struct'
+        dump['fields'] = self.fields
+        return dump
 
 class Table(object):
     ''' tables '''
     required_attributes = ['match_type', 'depth', 'request',
                            'response', 'operations']
     def __init__(self, table):
-        #print vars(table)
-        pass
+        match_types = {'P4_MATCH_EXACT': 'exact',
+                       'P4_MATCH_TERNARY': 'ternary'}
+        curr_types = {'P4_MATCH_TERNARY': 0,
+                      'P4_MATCH_EXACT': 0}
+        for field in table.match_fields:
+            curr_types[field[1].value] += 1
+        self.match_type = None
+        for key, value in curr_types.items():
+            if value != 0:
+                self.match_type = match_types[key]
+        assert self.match_type != None
+        self.depth = table.size
+        self.request = table.name + '_req_t'
+        self.response = table.name + '_resp_t'
+
+    def dump(self):
+        ''' dump table to yaml '''
+        dump = OrderedDict()
+        dump['type'] = 'table'
+        dump['match_type'] = self.match_type
+        dump['depth'] = self.depth
+        dump['request'] = self.request
+        dump['response'] = self.response
+        dump['operations'] = []
+        return dump
 
 class OtherModule(object):
     ''' other_module '''
@@ -62,17 +165,26 @@ class BasicBlock(object):
             self.instructions = None
             self.next_control_state = None
             #print vars(basicBlock)
+            #request action
+            #response actions
 
 class ControlFlow(object):
     ''' control_flow '''
     required_attributes = ['start_control_state']
     def __init__(self, controlFlow):
+        # offset
+        # basic_block
         pass
 
 class ProcessorLayout(object):
     ''' processor_layout '''
     required_attributes = ['format', 'implementation']
     def __init__(self, hlirParser):
+        # format: list
+        # implementation:
+        # - parser
+        # - ingress_control
+        # - deparser
         pass
 
 class OtherProcessor(object):
@@ -89,6 +201,8 @@ class MetaIR(object):
     def __init__(self, hlir):
         assert isinstance(hlir, HLIR)
         self.hlir = hlir
+        self.bir_yaml = OrderedDict()
+        self.field_width = OrderedDict()
 
         self.processor_layout = OrderedDict()
         self.tables = OrderedDict()
@@ -99,38 +213,84 @@ class MetaIR(object):
 
         self.construct()
 
-    def construct(self):
-        ''' HLIR -> BIR '''
-
-        # struct & metadata
+    def build_metadatas(self):
+        ''' create metadata object '''
         for hdr in self.hlir.p4_header_instances.values():
-            print 'header instances', hdr, hdr.max_index, hdr.metadata
             if hdr.metadata:
                 self.metadata[hdr.name] = Metadata(hdr)
-            else:
+            print 'metadata instance', hdr, hdr.metadata
+
+    def build_structs(self):
+        ''' create struct object '''
+        for hdr in self.hlir.p4_header_instances.values():
+            if not hdr.metadata:
                 self.structs[hdr.name] = Struct(hdr)
-            #FIXME handle stack
+            print 'header instances', hdr, hdr.max_index, hdr.metadata
 
-        # basic_block
-        for state in self.hlir.p4_parse_states.values():
-            self.basic_blocks[state.name] = BasicBlock(state)
+        for tbl in self.hlir.p4_tables.values():
+            field_width = OrderedDict()
+            for match_field in tbl.match_fields:
+                field = match_field[0]
+                field_width[field.name] = self.field_width[str(field)]
+            req = Struct(tbl, type="request", field=field_width)
+            self.structs[req.name] = req
 
-        # table
+        for tbl in self.hlir.p4_tables.values():
+            field_width = OrderedDict()
+            # table response fields
+            resp = Struct(tbl, type="response", field=field_width)
+            self.structs[resp.name] = resp
+
+    def build_tables(self):
+        ''' create table object '''
+        for tbl in self.hlir.p4_tables.values():
+            self.tables[tbl.name] = Table(tbl)
+
+    def build_basic_blocks(self):
+        ''' create basic blocks '''
         for tbl in self.hlir.p4_tables.values():
             self.basic_blocks[tbl.name] = BasicBlock(tbl)
-            self.tables[tbl.name] = Table(tbl)
-            self.structs[tbl.name] = Struct(tbl)
+
+        for state in self.hlir.p4_parse_states.values():
+            self.basic_blocks[state.name] = BasicBlock(state)
 
         for action in self.hlir.p4_actions.values():
             self.basic_blocks[action.name] = BasicBlock(action)
             print 'p4action', action
 
-        # control flow
+    def build_parsers(self):
+        ''' create parser and its states '''
+        pass
+
+    def build_match_actions(self):
+        ''' build match & action pipeline stage '''
+
+    def build_control_flows(self):
+        ''' build control flow '''
         for cond in self.hlir.p4_conditional_nodes.values():
             print 'conditional', cond
 
         for ingress in self.hlir.p4_ingress_ptr.keys():
             print 'ingress', ingress
+
+    def prepare_local_var(self):
+        ''' process hlir to setup commonly used variables '''
+        for hdr in self.hlir.p4_header_instances.values():
+            for field in hdr.fields:
+                path = ".".join([hdr.name, field.name])
+                self.field_width[path] = field.width
+
+    def construct(self):
+        ''' HLIR -> BIR '''
+
+        self.prepare_local_var() # local variables to simply style
+
+        self.build_structs()
+        self.build_metadatas()
+        self.build_tables()
+        self.build_basic_blocks()
+        self.build_match_actions()
+        self.build_control_flows()
 
         print self.hlir.p4_egress_ptr
 
@@ -142,8 +302,13 @@ class MetaIR(object):
 
     def pprint_yaml(self):
         ''' pretty print to yaml '''
-        pass
-
+        for name, inst in self.structs.items():
+            self.bir_yaml[name] = inst.dump()
+        for name, inst in self.metadata.items():
+            self.bir_yaml[name] = inst.dump()
+        for name, inst in self.tables.items():
+            self.bir_yaml[name] = inst.dump()
+        yaml.safe_dump(self.bir_yaml, sys.stdout, default_flow_style=False)
 
 def compile_hlir(hlir):
     ''' translate HLIR to BIR '''
@@ -153,9 +318,12 @@ def compile_hlir(hlir):
 
 def main():
     ''' entry point '''
-    argparser = argparse.ArgumentParser(description="P4FPGA P4 to Bluespec Compiler")
+    argparser = argparse.ArgumentParser(
+        description="P4FPGA P4 to Bluespec Compiler")
     argparser.add_argument('-f', '--file', required=True,
                            help='Input P4 program')
+    argparser.add_argument('-y', '--yaml', action='store_true', required=False,
+                           help="Output yaml")
     options = argparser.parse_args()
 
     hlir = HLIR(options.file)
@@ -163,5 +331,12 @@ def main():
 
     bir = compile_hlir(hlir)
 
+    if options.yaml:
+        bir.pprint_yaml()
+
 if __name__ == "__main__":
+    #hanw: fix pyyaml to print OrderedDict()
+    yaml.SafeDumper.add_representer(
+        OrderedDict, lambda dumper, value: represent_odict(
+            dumper, u'tag:yaml.org,2002:map', value))
     main()
