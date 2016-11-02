67c71b91d5fdc08944ee0972db7dc1bab00f9d77
Han Wang <hwang@cs.cornell.edu>
2016-11-02 07:48:35 -0400

    benchmark clean up

--- examples/benchmark/benchmark-action-128/app_init.cpp
+++ examples/benchmark/benchmark-action-128/app_init.cpp
@@ -2,14 +2,28 @@
 #include "MainRequest.h"
 
 void app_init (MainRequestProxy* device) {
-  ForwardTableReqT key = {0, 0x000cc47aa32535};
+  ForwardTableReqT key = {0, 0x003417eb96bf1c};
   ForwardTableRspT val = {1};
   fprintf(stderr, "insert table entry\n");
   device->forward_table_add_entry(key, val);
 
-  TestTblReqT udp_key = {0, 0x9091};
+  sleep(1);
+
+  key = {0, 0x000cc47aa32535};
+  val = {1};
+  fprintf(stderr, "insert table entry\n");
+  device->forward_table_add_entry(key, val);
+
+  TestTblReqT udp_key = {0, 0x4e20};
   TestTblRspT udp_val = {1};
   device->test_tbl_add_entry(udp_key, udp_val);
+
+  sleep(1);
+
+  udp_key = {0, 0x9091};
+  udp_val = {1};
+  device->test_tbl_add_entry(udp_key, udp_val);
+
 
 //  SendFrameReqT sendFrame_key = {1};
 //  SendFrameRspT sendFrame_val = {1, 0xaabbcceeddff};
