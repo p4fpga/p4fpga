2ec5f79273ac2b658779d04a6556af68dbedd494
Han Wang <hwang@cs.cornell.edu>
2017-03-31 09:42:29 -0700

    updated midend passes

--- compiler/src/midend.cpp
+++ compiler/src/midend.cpp
@@ -10,7 +10,13 @@
 #include "midend/removeParameters.h"
 #include "midend/local_copyprop.h"
 #include "midend/simplifyKey.h"
-#include "midend/simplifySelect.h"
+#include "midend/simplifySelectCases.h"
+#include "midend/simplifySelectList.h"
+#include "midend/validateProperties.h"
+#include "midend/compileTimeOps.h"
+#include "midend/predication.h"
+#include "midend/expandLookahead.h"
+#include "midend/tableHit.h"
 #include "frontends/p4/simplifyParsers.h"
 #include "frontends/p4/typeMap.h"
 #include "frontends/p4/evaluator/evaluator.h"
@@ -23,6 +29,27 @@
 #include "frontends/common/constantFolding.h"
 #include "frontends/p4/strengthReduction.h"
 #include "frontends/p4/uniqueNames.h"
+#include "midend/actionsInlining.h"
+#include "midend/actionSynthesis.h"
+#include "midend/convertEnums.h"
+#include "midend/copyStructures.h"
+#include "midend/eliminateTuples.h"
+#include "midend/local_copyprop.h"
+#include "midend/localizeActions.h"
+#include "midend/moveConstructors.h"
+#include "midend/nestedStructs.h"
+#include "midend/removeLeftSlices.h"
+#include "midend/removeParameters.h"
+#include "midend/removeReturns.h"
+#include "midend/simplifyKey.h"
+#include "midend/simplifySelectCases.h"
+#include "midend/simplifySelectList.h"
+#include "midend/validateProperties.h"
+#include "midend/compileTimeOps.h"
+#include "midend/predication.h"
+#include "midend/expandLookahead.h"
+#include "midend/tableHit.h"
+#include "midend/midEndLast.h"
 
 namespace FPGA {
 
@@ -60,20 +87,38 @@ const IR::P4Program* MidEnd::run(const FPGAOptions& options, const IR::P4Program
         new P4::InlineActions(&refMap, &typeMap),
         new P4::LocalizeAllActions(&refMap),
         new P4::UniqueNames(&refMap),
-        new P4::ClearTypeMap(&typeMap),
+        new P4::UniqueParameters(&refMap, &typeMap),
         new P4::SimplifyControlFlow(&refMap, &typeMap),
-        new P4::RemoveTableParameters(&refMap, &typeMap),
         new P4::RemoveActionParameters(&refMap, &typeMap),
         new P4::SimplifyKey(&refMap, &typeMap,
                             new P4::NonLeftValue(&refMap, &typeMap)),
-        new P4::RemoveExits(&refMap, &typeMap),
         new P4::ConstantFolding(&refMap, &typeMap),
-        new P4::SimplifySelect(&refMap, &typeMap, false),  // accept non-constant keysets
+        new P4::StrengthReduction(),
+        new P4::SimplifySelectCases(&refMap, &typeMap, true),  // require constant keysets
+        new P4::ExpandLookahead(&refMap, &typeMap),
         new P4::SimplifyParsers(&refMap),
         new P4::StrengthReduction(),
+        new P4::EliminateTuples(&refMap, &typeMap),
+        new P4::CopyStructures(&refMap, &typeMap),
+        new P4::NestedStructs(&refMap, &typeMap),
+        new P4::SimplifySelectList(&refMap, &typeMap),
+        new P4::Predication(&refMap),
+        new P4::ConstantFolding(&refMap, &typeMap),
         new P4::LocalCopyPropagation(&refMap, &typeMap),
+        new P4::ConstantFolding(&refMap, &typeMap),
         new P4::MoveDeclarations(),  // more may have been introduced
         new P4::SimplifyControlFlow(&refMap, &typeMap),
+        new P4::CompileTimeOperations(),
+        new P4::TableHit(&refMap, &typeMap),
+        new P4::MoveActionsToTables(&refMap, &typeMap),
+        new P4::TypeChecking(&refMap, &typeMap),
+        new P4::SimplifyControlFlow(&refMap, &typeMap),
+        new P4::RemoveLeftSlices(&refMap, &typeMap),
+        new P4::TypeChecking(&refMap, &typeMap),
+        new P4::ConstantFolding(&refMap, &typeMap, false),
+        new P4::TypeChecking(&refMap, &typeMap),
+        new P4::SimplifyControlFlow(&refMap, &typeMap),
+        new P4::RemoveAllUnusedDeclarations(&refMap),
         // evaluator,
     };
     midEnd.setName("MidEnd");
